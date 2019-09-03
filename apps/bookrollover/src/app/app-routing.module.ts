daa247c737713a93b92d18e02ed02a083dc630f5
Avi <avi@aravindh.me>
2019-09-03 08:31:31 -0400

    first commit

---
+++ apps/bookrollover/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes:Routes =[
+  {path:'/', component:HeaderComponent}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
